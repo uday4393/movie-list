fe0726e37f8fb5838283632a21050117cf4c82ae
Uday Kumar Mydam <udaym@Udays-MacBook-Pro.local>
2022-01-31 18:16:37 +0530

    Complete Movie List UI.

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <PopularMovies />
+    <UpcomingMovies />
+  </div>
+</template>
+
+<script>
+import PopularMovies from "../components/PopularMovies.vue";
+import UpcomingMovies from "../components/UpcomingMovies.vue";
+export default {
+  name: "Home",
+  components: {
+    PopularMovies,
+    UpcomingMovies,
+  },
+};
+</script>
